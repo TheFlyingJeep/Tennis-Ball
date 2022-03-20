e1383151686e0dccc4fff601801e7057e650b3bf
Aditron the greatslayer <letsgoawesome12345@gmail.com>
2022-03-20 01:21:32 -0400

    changed your mom

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from discord import FFmpegPCMAudio
 
 with open("token.txt") as t:
     token = t.read().strip()
-client = commands.Bot(command_prefix="hn ", case_insensitive=True)
+client = commands.Bot(command_prefix=["hn ","nh ","Hn ","hN ","Nh ","nH ","HN ","NH " ], case_insensitive=True)
 
-
+@client.command()
+async def hello(ctx):
+    await ctx.send("hi there")
 @client.event
 async def on_ready():
     print("Starting tennis ball bot!")
